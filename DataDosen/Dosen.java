d1db357bf4061651e3140ec63d546647adbb7f14
Giat00 <81604393+Giat00@users.noreply.github.com>
2021-04-02 14:56:32 +0800

    Menambahkan Input&Output Di Data Dosen

--- DataDosen/Dosen.java
+++ DataDosen/Dosen.java
@@ -1,11 +1,24 @@
 package DataDosen;
 
+import java.util.Scanner;
+
 public class Dosen{
  public static void  Tampilan(){
+
+ String nama;
+ int nidn;
+ 
+ Scanner scanner = new Scanner(System.in);
+
   System.out.println();
   System.out.println("======= Data Dosen ========");
-  System.out.println("NIDN : 1103058602");
-  System.out.println("Nama : Mirza Yogy Kurniawan");
+  System.out.print("Nama : ");
+  nama = scanner.nextLine();
+  System.out.print("NIDN : ");
+  nidn = scanner.nextInt();
   System.out.println("===========================");
+
+	System.out.println("Nama Anda"+nama);
+	System.out.println("NPM Anda"+nidn); 
  }
 }
